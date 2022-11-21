edc7f5834dc53d92a8baf9d324ae99c4e2c95046
Gonca Saygili <goncasaygili@gmail.com>
2022-11-21 23:03:54 +0300

    V2

--- src/main/java/yenidosya.java
+++ src/main/java/yenidosya.java
@@ -1,6 +1,10 @@
 public class yenidosya {
     public static void main(String[] args) {
 
+        System.out.println("Hello world!");
+        System.out.println("Git github ilk deneme");
+        System.out.println("Git github ikinci deneme");
+
     }
 
 }
